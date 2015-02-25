5c95bb5f37902e4bf14904b08c2cc1a95147c883
Lauricio Su <me@lauricio.com>
2015-02-25 17:51:44 +0700

    initial commit

---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  name: 'ox2:helpers',
+  summary: 'Various helpers',
+  version: '1.0.0',
+  git: ' /* Fill me in! */ '
+});
+
+var S = 'server';
+var C = 'client';
+var CS = [C, S];
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2.1');
+  // Core
+  api.use(['templating', 'session', 'tracker']);
+  // 3rd party
+  api.use([
+    'gadicohen:reactive-window@1.0.1'
+    ]);
+  api.addFiles('lib/oo-helpers.js', C);
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('ox2:helpers');
+  api.addFiles('tests/oo-helpers-tests.js');
+});
