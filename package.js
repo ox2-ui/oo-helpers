0f2f0d7b35c335ca6eaa7b7ecd8585162d074a69
Lauricio Su <me@lauricio.com>
2015-09-07 19:10:02 +0700

    removed reactive window

--- package.js
+++ package.js
@@ -12,11 +12,7 @@ var CS = [C, S];
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
   // Core
-  api.use(['templating', 'session', 'tracker']);
-  // 3rd party
-  api.use([
-    'gadicohen:reactive-window@1.0.4'
-    ]);
+  api.use(['templating', 'session']);
   api.addFiles('lib/oo-helpers.js', C);
 });
 
